c54adbbd11a41646b7ff5a1a6e0fe35f5e5cdce9
Taylor Plewe <taylor.plewe@awardco.com>
2025-04-15 21:24:01 -0600

    implement image placeholder for bad images, using Solid's <Show> element

--- src/EngineerSelection.tsx
+++ src/EngineerSelection.tsx
@@ -1,9 +1,17 @@
-import { Component, For, useContext } from "solid-js";
+import { Component, Show, createSignal, For, useContext } from "solid-js";
 import { Context } from "./Context.tsx";
 import shuffle from "./shuffle.ts";
 
 export const EngineerSelection: Component<{ engineerId: number }> = (props) => {
   const context = useContext(Context);
+
+  const [engineerIdsWithBadImageUrls, setEngineerIdsWithBadImageUrls] =
+    createSignal(new Set());
+  const addToEngineerIdsWithBadImageUrls = (id: number): void => {
+    const newSet = new Set(engineerIdsWithBadImageUrls());
+    newSet.add(id);
+    setEngineerIdsWithBadImageUrls(newSet);
+  };
 
   return (
     <>
@@ -16,7 +24,17 @@ export const EngineerSelection: Component<{ engineerId: number }> = (props) => {
           {(engineer) => (
             <li>
               <button>
-                <img src={engineer.imageUrl} />
+                <Show
+                  when={!engineerIdsWithBadImageUrls().has(engineer.id)}
+                  fallback={<div class="img-placeholder"></div>}
+                >
+                  <img
+                    src={engineer.imageUrl}
+                    onError={() =>
+                      addToEngineerIdsWithBadImageUrls(engineer.id)
+                    }
+                  />
+                </Show>
                 {engineer.name}
               </button>
             </li>
